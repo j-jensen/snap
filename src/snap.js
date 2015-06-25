4c77282346e630d7046e284220d5a847a67cc8f9
Jesper Jensen <jesperjensen358@gmail.com>
2015-06-25 23:21:12 +0200

    Trying to implement actor

---
+++ src/snap.js
@@ -0,0 +1,42 @@
+﻿define(['underscore'], function (_) {
+    var MODEL = window.Symbol ? window.Symbol() : '$$model',
+        EL = window.Symbol ? window.Symbol() : '$$el';
+
+    function Snap(config) {
+        config || (config = {});
+        _.extend(this, _.pick(config, ['initialize', 'render']));
+
+        this[MODEL] = _.result(config, 'model');
+        this.initialize.apply(this, arguments);
+    }
+
+    Snap.prototype = {
+
+        initialize: _.identity,
+
+        render: _.identity,
+
+        destroy: function () {
+        },
+
+        setModel: function (model) {
+            this[MODEL] = model;
+            this[EL] = this.render();
+        },
+
+        getModel: function () {
+            return this[MODEL];
+        }
+    };
+
+    return {
+        create: function (config) {
+            return new Snap(config);
+        },
+
+        render: function (snap, container) {
+            var el = snap.render();
+            container.appendChild(el);
+        }
+    };
+});
